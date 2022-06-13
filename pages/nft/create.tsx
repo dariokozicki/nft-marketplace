0bb26e02aeb6b12e392ed35e9bb68aa074c83cfe
Dario Kozicki <dariokozicki@gmail.com>
2022-06-13 15:02:36 -0300

    add link in navbar, remove transition

---
+++ pages/nft/create.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next";
+import BaseLayout from "../../components/layout/BaseLayout";
+
+const NftCreate: NextPage = () => {
+  return <BaseLayout>nft create</BaseLayout>;
+};
+
+export default NftCreate;
