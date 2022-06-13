8c192fd566a545127df1967a46a8ceb7f1e35aa8
Dario Kozicki <dariokozicki@gmail.com>
2022-06-13 11:33:56 -0300

    add nft item and list

---
+++ components/nft/list/index.tsx
@@ -0,0 +1,17 @@
+import NftItem, { NftMeta } from "../item";
+
+type NftListProps = {
+  nfts: NftMeta[];
+};
+
+const NftList = ({ nfts }: NftListProps) => {
+  return (
+    <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
+      {nfts.map((nft) => (
+        <NftItem key={nft.image} nft={nft} />
+      ))}
+    </div>
+  );
+};
+
+export default NftList;
