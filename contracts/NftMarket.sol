53b6185687ce71da92386d0eed8ff6e63788b6f8
Dario Kozicki <dariokozicki@gmail.com>
2022-06-15 15:35:54 -0300

    add erc721

--- contracts/NftMarket.sol
+++ contracts/NftMarket.sol
@@ -1,5 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract NFtMarket {
+contract NFtMarket is ERC721URIStorage {
+  constructor() ERC721("Creatures NFT", "CNFT") {
+
+  }
 }
